9959a6aa83c4a7282a73fbe26ec88230ba917ce8
royaljacquesytb <63215754+royaljacques@users.noreply.github.com>
2023-06-26 22:51:30 +0200

    update for wss https

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   }[]>([]);
 
   useEffect(() => {
-    const ws = new WebSocket('ws://api.jacqueskoenig.eclipscloud.fr');
+    const ws = new WebSocket('wss://api.jacqueskoenig.eclipscloud.fr');
 
     ws.onopen = () => {
       ws.send(JSON.stringify({
